faa63ac4cf889764f8b07753e5e1d8ac24be81d2
Philippe <philippe.latu@inetdoc.net>
2014-12-16 10:26:17 +0100

    Lecture d'un fichier texte

---
+++ readfile/readfile.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+const int MAXCHAR = 80;
+
+typedef char CHAINE80[MAXCHAR+1];
+
+int main() {
+
+	CHAINE80 ligne;
+	ifstream fichierEnLecture;
+
+	cout << "Donner le nom du fichier à lire" << endl;
+	cin >> ligne;
+
+	fichierEnLecture.open(ligne);
+	if (fichierEnLecture) {
+		do {
+			fichierEnLecture.getline(ligne, MAXCHAR);
+			cout << ligne << endl;
+		} while (! fichierEnLecture.eof());
+	}
+	else
+		cerr << "Ouverture du fichier " << ligne << " impossible" << endl;
+
+	return 0;
+}
